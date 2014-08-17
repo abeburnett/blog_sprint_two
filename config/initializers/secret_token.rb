b09059e541c35d31228bc55c19bd759827c32bba
Abraham R Burnett <abeburnett@gmail.com>
2014-08-17 00:43:57 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogSprintTwo::Application.config.secret_key_base = '36917a8cb05178c47b3c207b5c53b65b81508203c382d9dc4a7a860a480f841aaf8b872ada72346b81539c322ff22e656ff509c8d7cf8b7da25a7b5a8be88bd6'
